b62e0e4f10fe672fe3894a3627f3f5c2fc267bd6
imnllly <143996748+imnllly@users.noreply.github.com>
2023-11-20 13:05:49 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -19,22 +19,7 @@ string itc_reverse_str(string str) {
 	return str2;
 }
 
- /*string calculator(string str1, string str2) {
-	str1 = itc_reverse_str(str1);
-	str2 = itc_reverse_str(str2);
-	int tsch1 = 0;
-	int tsch2 = 0;
-	int tsch3 = 0;
-	int tsch_new = 0;
-	int x = 1;
-	for (int i = 0; i < itc_len(str1) && i < itc_len(str2); i--) {
-		tsch1 = str1[i] - '0';
-		tsch2 = str2[i] - '0';
-		tsch3 = (tsch1 + tsch2 + tsch_new) % 10;
-		tsch_new += x * tsch3;
-		x *= 10;
-	}
-} */
+
 
 void straight(string &str1, string &str2) {
 	int len1 = itc_len(str1);
@@ -49,7 +34,7 @@ void straight(string &str1, string &str2) {
 			str2 += '0';
 		}
 	}
-	
+
 }
 
 string calc_plus(string str1, string str2) {
@@ -60,26 +45,25 @@ string calc_plus(string str1, string str2) {
 	string str_new = "";
 	int o = 0;
 	char r = ' ';
-	for (int i = 0; i < len1 + 1; i++) {
+	for (int i = 0; i < len1; i++) {
 		tsch1 = str1[i] - '0';
 		tsch2 = str2[i] - '0';
-		tsch3 += o;
-		o = 0;
-		tsch3 = tsch1 + tsch2;
+		tsch3 = tsch1 + tsch2 + o;
+        o = 0;
 		if (tsch3 > 9) {
 			tsch3 %= 10;
 			o = 1;
 		}
-		r = tsch3 + '0';
+        r = tsch3 + '0';
 		str_new += r;
 	}
-	str_new = itc_reverse_str(str_new);
+	if(o == 1) str_new += '1';
 	return str_new;
 }
 
 int main()
 {
-	string str1, str2 = "";
+	string str1, str, res, str2 = "";
 	char op = ' ';
 	setlocale(LC_ALL, "RU");
 	cout << "Введите число 1: ";
@@ -88,13 +72,31 @@ int main()
 	cin >> str2;
 	cout << "Введите операцию (+/-): ";
 	cin >> op;
-	if(str1[0] == '-') string str3 = str1;
-	if(str2[0] == '-') string str4 = str2;
+	string str3 = "";
+	string str4 = "";
+    if(str1[0] == '-') {
+        for(int i = 1; i <= itc_len(str1); i++) str3 += str1[i];
+    }
+	if(str2[0] == '-') {
+        for(int i = 1; i <= itc_len(str2); i++) str4 += str2[i];
+    }
 	str1 = itc_reverse_str(str1);
 	str2 = itc_reverse_str(str2);
-	if (op == '+') {
-		cout << calc_plus(str1, str2);
+	str3 = itc_reverse_str(str3);
+	str4 = itc_reverse_str(str4);
+	if ((str1[0] != '-') && (str2[0] != '-') && (op == '+')) {
+		str = calc_plus(str1, str2);
+		cout << itc_reverse_str(str) << endl;
 	}
-
+	else if ((str1[0] != '-') && (str2[0] == '-') && (op == '-')) {
+		str = calc_plus(str1, str4);
+		cout << itc_reverse_str(str) << endl;
+	}
+    else if ((str1[0] == '-') && (str2[0] != '-') && (op == '+')) {
+		str = calc_plus(str1, str2) + "-";
+		// res = '-' + itc_reverse_str(str);
+		cout << itc_reverse_str(str) << endl;
+	}
+	else if(str1[0] == '-')
 	return 0;
 }
