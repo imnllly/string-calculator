204c85a0c7f5cfd062290b6279f5c807017a4d51
imnllly <143996748+imnllly@users.noreply.github.com>
2023-12-06 22:48:34 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -74,18 +74,34 @@ string calc_plus(string str1, string str2) {
 
 string calc_minus(string str1, string str2) {
 	int tsch1, tsch2, tsch3 = 0;
+	string str3 = "";
+	string str_new = "";
+	char min = ' ';
+	if (str1.size() < str2.size()) {
+		str3 = str1;
+		str1 = str2;
+		str2 = str3;
+		min = '-';
+	}
 	straight2(str1, str2);
 	str1 = itc_reverse_str(str1);
 	str2 = itc_reverse_str(str2);
 	long long len1 = str1.size();
 	long long len2 = str2.size();
 
-	string str_new = "";
 	int o = 0, j = 0, q = 0;
 	char r = ' ';
 	for (int i = 0; i < len1; i++) {
 		tsch1 = str1[i] - '0';
 		tsch2 = str2[i] - '0';
+		/*if (tsch1 >= tsch2) {
+			tsch3 = tsch1 - tsch2 - o;
+			o = 0;
+		}
+		else if (tsch2 > tsch1) {
+			tsch3 = tsch1 - tsch2 + o + 10;
+			o = 1;
+		} */
 		tsch3 = tsch1 - tsch2 - o + q;
 		q = 0;
 		if (tsch2 > tsch1) {
@@ -95,14 +111,13 @@ string calc_minus(string str1, string str2) {
 				q = 10;
 				o = 1;
 			}
-
 		} 
-		else o = 0;
+		else o = 0; 
 		
 		r = tsch3 + '0';
 		str_new += r;
 	}
-	
+	str_new += min;
 	return str_new;
 }
 
@@ -111,8 +126,7 @@ int main()
 	string str1, str, res, str2 = "";
 	char op = ' ';
 	setlocale(LC_ALL, "RU");
-	cout << itc_reverse_str(calc_minus("150", "30"));
-	/*cout << "Введите число 1: ";
+	cout << "Введите число 1: ";
 	cin >> str1;
 	cout << "Введите число 2: ";
 	cin >> str2;
@@ -120,36 +134,63 @@ int main()
 	cin >> op;
 	string str3 = "";
 	string str4 = "";
-	/* if (str1[0] == '-') {
-		for (int i = 1; i <= itc_len(str1); i++) str3 += str1[i];
+	 if (str1[0] == '-') {
+		for (int i = 1; i < str1.size(); i++) str3 += str1[i];
 	}
 	if (str2[0] == '-') {
-		for (int i = 1; i <= itc_len(str2); i++) str4 += str2[i];
+		for (int i = 1; i < str2.size(); i++) str4 += str2[i];
 	} 
-	str1 = itc_reverse_str(str1);
-	str2 = itc_reverse_str(str2);
-	str3 = str1.erase(0, 1);
-	str4 = str2.erase(0, 1);
 	if (op == '+') {
 		if ((str1[0] != '-') && (str2[0] != '-')) {
+			str1 = itc_reverse_str(str1);
+			str2 = itc_reverse_str(str2);
 			str = calc_plus(str1, str2);
-			cout << itc_reverse_str(str) << endl; // a + b
+			cout << itc_reverse_str(str) << endl; // a + b, работает
 		}
 		else if ((str1[0] == '-') && (str2[0] == '-')) {
-			str1 = str1.erase(0, 1);
-			str2 = str2.erase(0, 1);
-			str = calc_plus(str1, str2) + "-";
-			// res = '-' + itc_reverse_str(str);
-			cout << itc_reverse_str(str) << endl;
+			str3 = itc_reverse_str(str3);
+			str4 = itc_reverse_str(str4);
+			str = calc_plus(str3, str4);
+			str = itc_reverse_str(str);
+			str = '-' + str; 
+			cout << str << endl; // -a + -b, работает
 		}
-
+		else if ((str1[0] == '-') && (str2[0] != '-')) {
+			str1 = itc_reverse_str(str1);
+			str4 = itc_reverse_str(str4);
+			str = calc_minus(str4, str1);
+			cout << itc_reverse_str(str) << endl; // -a + b, работает
+		}
+		else if ((str1[0] != '-') && (str2[0] == '-')) {
+			str = calc_minus(str1, str4);
+			cout << itc_reverse_str(str) << endl; // a + (-b), работает 
+		}
 	}
-/*
-	else if ((str1[0] != '-') && (str2[0] == '-') && (op == '-')) {
-		str = calc_plus(str1, str4);
-		cout << itc_reverse_str(str) << endl;
+
+	else if (op == '-') {
+		if ((str1[0] != '-') && (str2[0] != '-')) {
+			str = calc_minus(str1, str2);
+			cout << itc_reverse_str(str) << endl; // a - b, работает
+		}
+		else if ((str1[0] != '-') && (str2[0] == '-')) {
+			str1 = itc_reverse_str(str1);
+			str4 = itc_reverse_str(str4);
+			str = calc_plus(str1, str4);
+			cout << itc_reverse_str(str) << endl; // a - (-b), работает
+		}
+		else if ((str1[0] == '-') && (str2[0] != '-')) {
+			str3 = itc_reverse_str(str3);
+			str2 = itc_reverse_str(str2);
+			str = calc_plus(str3, str2);
+			cout << '-' + itc_reverse_str(str) << endl; // -a - b, работает
+		}
+		else if ((str1[0] == '-') && (str2[0] == '-')) {
+			str3 = itc_reverse_str(str3);
+			str4 = itc_reverse_str(str4);
+			str = calc_minus(str4, str3);
+			cout << itc_reverse_str(str) << endl; // -a - (-b), работает
+		}
 	}
 	
-	else if (str1[0] == '-') */
 		return 0;
-}
+} 
